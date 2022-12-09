285ea59507574a193fdd00c1eb94b3280911c23e
Taha <enaayahdeveloper012@gmail.com>
2022-12-09 18:24:37 +0530

    modified in script.cs

---
+++ script.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Generic.Collections;
+
+//Dummy Script
+
+public class Test
+{
+    void Update()
+    {
+        Debug.Log("name");
+    }
+}
