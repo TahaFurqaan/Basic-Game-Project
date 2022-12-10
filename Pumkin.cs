cbe6b6b6755badaf371f5161d6e3ac6bdd226252
Taha Furqaan <120082196+TahaFurqaan@users.noreply.github.com>
2022-12-10 16:50:08 +0530

    Update Pumkin.cs

--- Pumkin.cs
+++ Pumkin.cs
@@ -13,12 +13,6 @@ public class Pumkin : MonoBehaviour
         myBody = GetComponent<Rigidbody>();
     }
     
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
